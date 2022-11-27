40354e43fd89f0155dc480f5eea8d65c79755533
King <king@kali.King>
2022-11-27 14:32:44 +0330

    complate

--- drf/settings.py
+++ drf/settings.py
@@ -147,7 +147,6 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
-
 SPECTACULAR_SETTINGS = {
     'TITLE': 'Your Project API',
     'DESCRIPTION': 'Your project description',
