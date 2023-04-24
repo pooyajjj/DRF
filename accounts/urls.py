c2561bfe484b63ff523278198c154709d0435a96
pooya-dev <pooya.jv@gmail.com>
2023-04-24 10:56:14 +0330

    working clean

--- accounts/urls.py
+++ accounts/urls.py
@@ -19,3 +19,8 @@ router.register('user', views.UserViewSet)
 urlpatterns += router.urls
 
 
+
+# {
+# 	"refresh": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTY3ODE4MTcxMSwiaWF0IjoxNjc4MDk1MzExLCJqdGkiOiIyYjU1YWFlYWJmOGE0YzQ2YWFhNjY3MjkyOWMxYjBmYSIsInVzZXJfaWQiOjF9.ZNPdZ7x1wNYJL1ozF7ybqv1DFa72M8__xhIRJyXN-Qs",
+# 	"access": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjc4MDk1NjExLCJpYXQiOjE2NzgwOTUzMTEsImp0aSI6IjM2MmUzMDY2N2NiMDRkMGJhMWMyNTJiZGE3OGU5YWM5IiwidXNlcl9pZCI6MX0.oUK06DwzBpZMJkfBf9fH7CbsR2YWCV5DQd8JVIK334E"
+# }
